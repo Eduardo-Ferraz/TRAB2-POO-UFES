f477c7344a327ff8382d7f764fa145e20270f3c5
Eduardo-Ferraz <eduardoc.ferraz2@gmail.com>
2023-02-01 12:42:40 -0300

    feat: parse_args

--- main.py
+++ main.py
@@ -2,16 +2,16 @@
 from src.datasets.dataset_factory import create_dataset
 from src.classifiers.classifier_factory import create_classifier
 from src.experiment import Experiment
+from src.io.args import parse_args
 from src.io.config import load_config
 from src.io.report import write_report
 
-from sys import argv
-
 
 def main():
-    CONFIG_PATH = './data/configs/'
-
-    config = load_config(CONFIG_PATH + argv[1])
+    # obter os nomes dos arquivos de configuracao e de saida da linha de comando
+    args = parse_args()
+    # le o arquivo json e retorna como um dicionario
+    config = load_config('./data/configs/'+args.config_path)
 
     train_dataset = create_dataset(config["train_path"], config["type"])
     test_dataset = create_dataset(config["test_path"], config["type"])
@@ -21,7 +21,7 @@ def main():
     metrics = experiment.run(classifier)
 
     # escreve o arquivo de saida
-    write_report(CONFIG_PATH + argv[2], config, metrics)
+    write_report(args.report_path, config, metrics)
 
     print(metrics['accuracy'])
     print("Success.")
