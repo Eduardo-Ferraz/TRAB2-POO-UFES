81e9fdb09f0f5c1cc24daca5e9b6b5c5e65b220e
Eduardo-Ferraz <eduardoc.ferraz2@gmail.com>
2023-02-06 11:31:41 -0300

    refactor: Parse args

--- src/io/args.py
+++ src/io/args.py
@@ -4,14 +4,9 @@ from typing import Any
 
 def parse_args() -> Any:
     """ le os argumentos de linha de comando usando a biblioteca argparse """
-    parser = argparse.ArgumentParser(
-                    prog = 'ProgramName',
-                    description = 'What the program does',
-                    epilog = 'Text at the bottom of help')
+    parser = argparse.ArgumentParser()
     parser.add_argument('config_path', type=str, help='Path das configs')
     parser.add_argument('report_path', type=str, help='Path do report.txt')
     args = parser.parse_args()
 
-
-    # Exemplo. Utilizar o argparse na versao final
     return args
