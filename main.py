31585651ed35269ae7c05962d267217091b897a3
Igor Baiocco <igor64br@gmail.com>
2023-02-08 12:30:04 -0300

    refact: ajustes finais (1)

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ def main():
     # obter os nomes dos arquivos de configuracao e de saida da linha de comando
     args = parse_args()
     # le o arquivo json e retorna como um dicionario
-    config = load_config('./data/configs/'+args.config_path)
+    config = load_config(args.config_path)
 
     train_dataset = create_dataset(config["train_path"], config["type"])
     test_dataset = create_dataset(config["test_path"], config["type"])
@@ -22,9 +22,6 @@ def main():
 
     # escreve o arquivo de saida
     write_report(args.report_path, config, metrics)
-
-    print(metrics['accuracy'])
-    print("Success.")
 
 if __name__ == "__main__":
     main()
